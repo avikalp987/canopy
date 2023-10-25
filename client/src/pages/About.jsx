b3e9370936d765e8b0f530bff798daf2489531b0
vikalp <avikalp233@gmail.com>
2023-10-25 14:56:50 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='mt-7 p-3'>
+      <h1 className='text-slate-700 text-3xl font-bold'>About Canopy : </h1>
+      <p className='mt-1'>Canopy was my first well organised project developed in october 2023</p>
+    </div>
   )
 }
